6e9799d151b67aaa03a955e21971c5fb47f3ef97
Carron Muleya <carronmuleya10@gmail.com>
2024-05-31 11:53:09 +0200

    hh

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     # Start scheduler on new Thread
     # context = ('/etc/letsencrypt/live/tekon.co.zw/fullchain.pem', '/etc/letsencrypt/live/tekon.co.zw/privkey.pem')#certificate and key files
     # app.run(debug=True, ),
-    app.run(debug=True,host="0.0.0.0",port=5000,ssl_context=context)
+    app.run(debug=True,host="0.0.0.0",port=5000)
